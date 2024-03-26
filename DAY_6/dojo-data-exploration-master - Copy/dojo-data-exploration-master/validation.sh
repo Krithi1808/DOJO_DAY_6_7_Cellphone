ed004ef4ca8b62a797bfa39246e4f4861499444f
Krithi1808 <72186560+Krithi1808@users.noreply.github.com>
2024-03-26 08:59:04 -0400

    Delete validation.sh

--- DAY_6/dojo-data-exploration-master - Copy/dojo-data-exploration-master/validation.sh
+++ DAY_6/dojo-data-exploration-master - Copy/dojo-data-exploration-master/validation.sh
@@ -1,9 +1,1 @@
-#!/bin/sh
-projloc=~/Desktop/Project/dojo-data-exploration
-validationloc=~/Desktop/Project/projectvalidation/dojo-data-exploration
-#Copy the scoring code to the project location
-cp ${validationloc}/score.sh ${projloc}/.
-cd ${projloc}
-if [ -e score.sh ];then
-    sh score.sh
-fi
+
